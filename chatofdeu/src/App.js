7a163cfa4c1ef52403d3f9303c2e88fb4ed37bb2
thelight0804 <thelight0804@gmail.com>
2023-08-18 20:00:46 +0900

    Add menu page

--- chatofdeu/src/App.js
+++ chatofdeu/src/App.js
@@ -4,16 +4,54 @@ import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Menu from "../src/page/Menu";
 import Facility from "../src/page/Facility";
+import { func } from "prop-types";
 
 function App() {
-    return(
-        <>
-            <Routes>
-                <Route path="/" element={<Menu/>} />
-                <Route path="/facility" element={<Facility/>} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <div class="header">
+        <div class="title">Menu</div>
+      </div>
+        <Navi/>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/facility" element={<Facility />} />
+      </Routes>
+    </>
+  );
+}
+
+function Navi() {
+  return (
+    <div className="navBar">
+      <div class="indicators">
+        <div id="i1">
+          <div class="navi-indicator" id="ni1"></div>
+        </div>
+        <div id="i2">
+          <div class="navi-indicator" id="ni2"></div>
+        </div>
+      </div>
+      <div class="navi">
+        <div class="navi-item1">
+          <button class="nav-button" id="startersbutton">
+            <div class="navi-icon">
+              <i class="fas fa-seedling"></i>
+            </div>
+            <div class="navi-text">Starters</div>
+          </button>
+        </div>
+        <div class="navi-item2">
+          <button class="nav-button" id="mainsbutton">
+            <div class="navi-icon">
+              <i class="fas fa-pizza-slice"></i>
+            </div>
+            <div class="navi-text">Mains</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- chatofdeu/src/page/Menu.js
+++ chatofdeu/src/page/Menu.js
@@ -1,9 +1,36 @@
-function Menu(){
-    return(
-        <>
-            <h1>Menu</h1>
-        </>
-    );
+import React from "react";
+import "./../css/Menu.css";
+
+function Menu() {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.11.1/css/all.css"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Rubik:wght@300;700&display=swap"
+        rel="stylesheet"
+      />
+
+      <div class="container">
+        <div class="menu">
+          <div class="menu-separator">Separator</div>
+          <div class="menu-item">
+            <div class="menu-item-name">Food</div>
+            <div class="menu-item-description">Food yada yada</div>
+            <div class="menu-item-price">17.50</div>
+          </div>
+          <div class="menu-separator">Separator</div>
+          <div class="drink-item">
+            <div class="drink-item-name">Drink</div>
+            <div class="drink-item-description">3.3dl</div>
+            <div class="drink-item-price">4.50</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Menu;
