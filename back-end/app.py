29c69fe5b00a1a0baa4ba13f2412e051ed930a69
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-08-18 18:27:00 +0900

    openai.py 생성
    pip install openai

--- back-end/app.py
+++ back-end/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 # 메인 페이지(홈 페이지) 라우팅/ 리퀘스트 방법 GET, POST
@@ -10,4 +11,4 @@ def home():
 
 # debug 모드로 실행
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080) 
