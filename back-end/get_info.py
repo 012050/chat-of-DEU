6ac9c5de97fa7672e81865d4d4cbff3035acded1
012050 <beomjunchoe@naver.com>
2023-08-18 22:31:51 +0900

    복지문화시설 파싱

--- back-end/get_info.py
+++ back-end/get_info.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup as bs
+
 
 def get_food():
     r = requests.get('https://raw.githubusercontent.com/asw-dod/Deu_food_api/master/output/api.json')
@@ -8,5 +10,18 @@ def get_food():
     else:
         return r.status_code
 
+def school_info():
+    html_text = requests.get('https://www.deu.ac.kr/www/content/57')
+
+    html = bs(html_text.text, 'html.parser')
+    html = html.find("tbody")
+    html = html.select("tr")
+    return html
+    # if r.status_code == 200:
+    #     return r.json()
+    # else:
+    #     return r.status_code
+
+
 if __name__ == "__main__":
-    print(get_food())
+    print(school_info())
