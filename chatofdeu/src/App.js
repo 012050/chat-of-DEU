fc32523b6c98e574c13d92a952146b1acc191e43
thelight0804 <thelight0804@gmail.com>
2023-08-18 20:25:37 +0900

    Add nav bar

--- chatofdeu/src/App.js
+++ chatofdeu/src/App.js
@@ -1,57 +1,48 @@
-import React from "react";
-import "./App.css";
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
-import Menu from "../src/page/Menu";
-import Facility from "../src/page/Facility";
-import { func } from "prop-types";
+    import React from "react";
+    import { Routes, Route } from "react-router-dom";
+
+    import "./App.css";
+    import Menu from "../src/page/Menu";
+    import Facility from "../src/page/Facility";
+
+    import Box from "@mui/material/Box";
+    import BottomNavigation from "@mui/material/BottomNavigation";
+    import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+    import RestoreIcon from "@mui/icons-material/Restore";
+    import FavoriteIcon from "@mui/icons-material/Favorite";
+    import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 function App() {
-  return (
+    
+    return (
     <>
-      <div class="header">
+        <div class="header">
         <div class="title">Menu</div>
-      </div>
-        <Navi/>
-      <Routes>
-        <Route path="/" element={<Menu />} />
+        </div>
+
+        <Navi /> 
+        <Routes>
+        {/* <Route path="/" element={<Menu />} /> */}
         <Route path="/facility" element={<Facility />} />
-      </Routes>
+        </Routes>
     </>
-  );
-}
+    );
+    }
 
-function Navi() {
-  return (
-    <div className="navBar">
-      <div class="indicators">
-        <div id="i1">
-          <div class="navi-indicator" id="ni1"></div>
-        </div>
-        <div id="i2">
-          <div class="navi-indicator" id="ni2"></div>
-        </div>
-      </div>
-      <div class="navi">
-        <div class="navi-item1">
-          <button class="nav-button" id="startersbutton">
-            <div class="navi-icon">
-              <i class="fas fa-seedling"></i>
-            </div>
-            <div class="navi-text">Starters</div>
-          </button>
-        </div>
-        <div class="navi-item2">
-          <button class="nav-button" id="mainsbutton">
-            <div class="navi-icon">
-              <i class="fas fa-pizza-slice"></i>
-            </div>
-            <div class="navi-text">Mains</div>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
+    function Navi() {
+        let [value, setValue] = React.useState(0);
+    return <>
+        <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+            setValue(newValue);
+            }}
+        >
+            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        </BottomNavigation>
+    </>;
+    }
 
-export default App;
+    export default App;
--- chatofdeu/src/index.js
+++ chatofdeu/src/index.js
@@ -1,3 +1,7 @@
+//npm install @mui/material @emotion/react @emotion/styled
+//npm install @mui/material @mui/styled-engine-sc styled-components
+//npm install @fontsource/roboto
+//npm install @mui/icons-material
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
