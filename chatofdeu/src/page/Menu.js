bcd00ecadf095f14bc6b1512f01b16e87fccc8ef
thelight0804 <thelight0804@gmail.com>
2023-08-18 23:46:49 +0900

    Add menu card

--- chatofdeu/src/page/Menu.js
+++ chatofdeu/src/page/Menu.js
@@ -2,22 +2,31 @@ import { useState } from "react";
 import "./../css/Menu.css"
 
 function Menu(){
-    let [meal, setMeal] = useState(["신라면, 김치", "안성탕면, 단무지", "신라면, 계란후라이"]);
+    let [data, setData] = useState([
+        {restaurant: "행복 기숙사", meal: ["아침", "점심", "저녁"]},
+        {restaurant: "효민 기숙사", meal: ["아침", "점심", "저녁"]},
+        {restaurant: "수덕전", meal: ["아침", "점심", "저녁"]},
+        {restaurant: "정보공학관", meal: ["아침", "점심", "저녁"]},
+        {restaurant: "국제관", meal: ["아침", "점심", "저녁"]},
+        {restaurant: "지천관", meal: ["아침", "점심", "저녁"]},
+    ])
     return(
         <div className="container">
             <h1>식단</h1>
-            <MealCard meal = {meal}/>
+            {data.map((r, i) => <MealCard i={i} data={data}/>)}
         </div>
     );
 }
 
-function MealCard({meal}){
+function MealCard({data, i}){
     return(
         <div className="mealCard">
-            <h4>행복기숙사</h4>
-            <p>아침 : {meal[0]}</p>
-            <p>점심 : {meal[1]}</p>
-            <p>저녁 : {meal[2]}</p>
+            <div className="mealDetail">
+                <h4>{data[i].restaurant}</h4>
+                <p>아침 : {data[i].meal[0]}</p>
+                <p>점심 : {data[i].meal[1]}</p>
+                <p>저녁 : {data[i].meal[2]}</p>
+            </div>
         </div>
     )
 }
