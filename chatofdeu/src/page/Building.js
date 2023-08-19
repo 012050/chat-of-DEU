b7f6d105ba126f3c92b396a74680cf52e3406228
thelight0804 <thelight0804@gmail.com>
2023-08-19 10:37:36 +0900

    Add change language at building select

--- chatofdeu/src/page/Building.js
+++ chatofdeu/src/page/Building.js
@@ -14,22 +14,50 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 
 function Building(){
-    // let [selectLan, setSelect] = useState({
-    //     country: "ko",
-    //     restaurant: [
-    //         "식당",
-    //         "카페",
-    //         "스터디",
-    //         "ATM",
-    //     ]
-    // });
     let [select, setSelect] = useState("식당"); //선택 값
     let [data, setData] = useState([]);
+
+    let [buildingName, setBuildingName] = useState([
+        {
+            country: "ko",
+            building: "건물 번호",
+            selects: ["식당", "카페", "스터디", "ATM", "편의점", "편의시설"]
+        },
+        {
+            country: "en",
+            building: "Building number",
+            selects: ["Restaurant", "Cafe", "Study Area", "ATM", "Convenience Store", "Facilities"]
+        },
+        {
+            country: "ja",
+            building: "ビル番号",
+            selects: ["レストラン", "カフェ", "スタディエリア", "ATM", "コンビニ", "施設"]
+        },
+        {
+            country: "vi",
+            building: "số tòa nhà",
+            selects: ["Nhà hàng", "Quán cà phê", "Khu vực học", "Máy ATM", "Cửa hàng tiện lợi", "Tiện ích"]
+        },
+        {
+            country: "zh-CN",
+            building: "楼号",
+            selects: ["餐厅", "咖啡馆", "自习区", "自动提款机", "便利店", "便利设施"]
+        },
+    ])
+
+    
+
+    const [translatedData, setTranslatedData] = useState(buildingName[0])
+
     
     let language = useSelector((state) => state.language.value); //언어 정보
     let dispatch = useDispatch();
 
+
     useEffect(()=>{
+        let num = buildingName.findIndex((element => element.country === language));
+        setTranslatedData(buildingName[num]);
+
         axios.post('http://minimalist.iptime.org:8080/building/info', {}, {params:{
             keyword: `${select}`,
             lan_type: `${language}`
@@ -56,8 +84,8 @@ function Building(){
           src={process.env.PUBLIC_URL + "/img/camplus-map.jpg"}
           width="100%"
         />
-        <BuildingRadio select = {select} setSelect = {setSelect}/>
-        {data.map((r, i) => <BuildingCard key={i} i={i} data={data}/>)}
+        <BuildingRadio select = {select} setSelect = {setSelect} translatedData = {translatedData}/>
+        {data.map((r, i) => <BuildingCard key={i} i={i} data={data}  translatedData = {translatedData}/>)}
       </div>
     );
 }
@@ -67,7 +95,7 @@ function Building(){
  * @param select 선택값
  * @param setSelect 선택값 설정 함수
  */
-function BuildingRadio({select, setSelect}){
+function BuildingRadio({select, setSelect, translatedData}){
     const handleChange = (event) => {
         setSelect(event.target.value);
     };
@@ -82,22 +110,22 @@ function BuildingRadio({select, setSelect}){
             >
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, md: 12 }}>
                 <Grid item xs={2} md={4}>
-                    <FormControlLabel value="식당" control={<Radio />} label="식당" />
+                    <FormControlLabel value="식당" control={<Radio />} label={translatedData.selects[0]} />
                 </Grid>
                 <Grid item xs={2} md={4}>
-                    <FormControlLabel value="카페" control={<Radio />} label="카페" />
+                    <FormControlLabel value="카페" control={<Radio />} label={translatedData.selects[1]} />
                 </Grid>
                 <Grid item xs={2} md={4}>
-                    <FormControlLabel value="스터디 공간" control={<Radio />} label="스터디" />
+                    <FormControlLabel value="스터디 공간" control={<Radio />} label={translatedData.selects[2]} />
                 </Grid>
                 <Grid item xs={2} md={4}>
-                    <FormControlLabel value="ATM" control={<Radio />} label="ATM" />
+                    <FormControlLabel value="ATM" control={<Radio />} label={translatedData.selects[3]} />
                 </Grid>
                 <Grid item xs={2} md={4}>
-                    <FormControlLabel value="편의점" control={<Radio />} label="편의점" />
+                    <FormControlLabel value="편의점" control={<Radio />} label={translatedData.selects[4]} />
                 </Grid>
                 <Grid item xs={2} md={4}>
-                    <FormControlLabel value="편의시설" control={<Radio />} label="편의시설" />
+                    <FormControlLabel value="편의시설" control={<Radio />} label={translatedData.selects[5]} />
                 </Grid>
         </Grid>
         </RadioGroup>
@@ -112,14 +140,15 @@ function BuildingRadio({select, setSelect}){
  * @param data 건물 시설 정보
  * @returns 
  */
-function BuildingCard({i, data}){
+function BuildingCard({i, data, translatedData}){
+    console.log(translatedData)
     return(
-        <div className="mealCard">
-            <div className="mealDetail">
-                <h3>{data[i][1]}</h3>
-                <p><b>건물번호 : </b>{data[i][0]}</p>
+            <div className="mealCard">
+                <div className="mealDetail">
+                    <h3>{data[i][1]}</h3>
+                    <p><b>{translatedData.building} : </b>{data[i][0]}</p>
+                </div>
             </div>
-        </div>
     )
 }
 
